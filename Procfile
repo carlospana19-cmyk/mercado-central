cf99b1e765cc51afdf15ebb8688f4783390e7055
carlospana19-cmyk <carlos_pana19@hotmail.com>
2026-05-21 22:12:27 -0500

    config: preparar servidor IA para Render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ia_server:app
--- ia_server.py
+++ ia_server.py
@@ -89,5 +89,6 @@ def optimizar():
         return jsonify({"error": "Error servidor"}), 500
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
 
